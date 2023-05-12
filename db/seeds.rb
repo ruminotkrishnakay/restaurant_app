28e61b5e652ab01b8883915e30bf03894f790324
ruminotkrishnakay <kkruminot@miuandes.cl>
2023-05-12 17:12:53 -0400

    add elements

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,9 @@ Restaurant.create(name:"Mcdonald",address:"Las Flores 7012, Las Condes",id:52,cr
 Restaurant.create(name:"Casa China",address:"Avenida Las Condes 1723, Las Condes",id:53,created_at: 12-05-23, updated_at:12-05-23)
 Restaurant.create(name:"Don Beto",address:"Francisco de Asis 1023, Las Condes",id:54,created_at: 12-05-23, updated_at:12-05-23)
 
-Item.create(name:"Filete mignon",price:12990,id:10,calories: 435 ,created_at: 12-05-23, updated_at:12-05-23)
-Item.create(name:"Lechuga, palmito. palta",price:5690,id:11,calories: 320,created_at: 12-05-23, updated_at:12-05-23)
-Item.create(name:"Agua sin gas",price:2190,id:12,calories:0 ,created_at: 12-05-23, updated_at:12-05-23)
-Item.create(name:"café",price:2890,id:13,calories:10 ,created_at: 12-05-23, updated_at:12-05-23)
+Item.create(name:"Filete mignon",price:12990,id:10,calories:435,created_at:12-05-23,updated_at:12-05-23)
+Item.create(name:"Lechuga, palmito palta",price:5690,id:11,calories:320,created_at:12-05-23,updated_at:12-05-23)
+Item.create(name:"Agua sin gas",price:2190,id:12,calories:0,created_at:12-05-23,updated_at:12-05-23)
+Item.create(name:"café",price:2890,id:13,calories:10,created_at:12-05-23,updated_at:12-05-23)
 Item.create(name:"Torta 4 leches",price:4190,id:14,calories:510,created_at: 12-05-23, updated_at:12-05-23)
 
